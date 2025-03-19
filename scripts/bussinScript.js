6baa89e8d4046551fc2fa3db0fcb578b1cb38a46
obunga99 <110899027+obunga99@users.noreply.github.com>
2025-03-19 10:20:15 +0800

    Add files via upload
    222222

---
+++ scripts/bussinScript.js
@@ -0,0 +1,30 @@
+
+
+function removeButt()
+{
+    let element = document.getElementById("but").style.left = "-90px";
+    let element2 = document.getElementById("return").style.left = "170px";
+    
+    let nav = document.getElementById("bar").style.left = "0%";
+    
+    
+}
+
+function addButt()
+{
+    let element = document.getElementById("but").style.left = "10px";
+    
+    
+
+    document.getElementById("return").style.left = "-900px";
+    
+    
+    
+   
+    document.getElementById("bar").style.left = "-900px";
+    
+    
+    
+}
+
+
